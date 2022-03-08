a0ed6669cde2cf4d2c86e6957ace97753bf1b532
EC2 Default User <ec2-user@ip-172-31-5-25.ec2.internal>
2022-03-08 05:28:17 +0000

    [add]検索機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,5 +38,20 @@ has_many :followers, through: :reverse_of_relationships, source: :follower
   def following?(user)
     followings.include?(user)
   end
-
+  
+  # 検索機能
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users 
   root :to =>"homes#top"
   get "home/about"=>"homes#about", as: 'about'
+  get "search"=>"searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]do
     resources :book_comments, only: [:create, :destroy]
