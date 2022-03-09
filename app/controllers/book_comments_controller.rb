0e840e9be729fafac2b69c6488b8fddf0c862b70
EC2 Default User <ec2-user@ip-172-31-5-25.ec2.internal>
2022-03-09 12:18:33 +0000

    [add]コメント機能の非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,20 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comments_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comments_params)
+    @comment.book_id = @book.id
+    @comment.save
+    #redirect_to request.referer
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @comment.book_id = @book.id
+    @comment.destroy
+    #redirect_to request.referer
+    #renderしたときに@postのデータがないので@postを定義
+
   end
 
   private
