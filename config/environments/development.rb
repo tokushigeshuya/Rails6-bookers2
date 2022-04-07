8dedac18958760394ccc763ad64df42657be1867
tokushigeshuya <narihare1130@gmail.com>
2022-04-07 10:21:37 +0000

    [firstcommit]add gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "610399686eb94ddcb86ba8bbb7ca3f80.vfs.cloud9.us-east-1.amazonaws.com"
+
+  config.hosts << "ee7ceb1ba5a946beb74edf4f99608b6c.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
